c3d3b6ca9d6077ff59ecb0495926a2525711de64
ronitsinha <ronittheawesome@gmail.com>
2019-07-22 13:56:46 -0400

    Messing around with test 4

--- test4.py
+++ test4.py
@@ -1,5 +1,14 @@
 from WeDo2 import *
+from bleak import BleakClient
+import asyncio
 
-h = HubManager(1)
+# h = HubManager(1)
 
-h.wedos[0].set_led_mode(LED_DISCREET_MODE)
+# h.wedos[0].set_led_mode(LED_DISCREET_MODE)
+# h.wedos[0].set_led_color(255,0,0)
+
+async def run():
+	await BleakClient(address='24:71:89:17:9D:AE', loop=loop).connect()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
