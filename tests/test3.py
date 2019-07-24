54d982294ccd3fe77b1e94d7731eb86701f0bfde
ronitsinha <ronittheawesome@gmail.com>
2019-07-24 09:23:53 -0400

    move tests into subdir

--- tests/test3.py
+++ tests/test3.py
@@ -12,10 +12,33 @@ PORT_INFO_UUID = '00001527-1212-efde-1523-785feabcd123'
 INPUT_COMMAND_UUID = '00001563-1212-efde-1523-785feabcd123'
 OUTPUT_COMMAND_UUID = '00001565-1212-efde-1523-785feabcd123'
 
-outputted_value = 1;
+GLOBAL_Q = []
+
+outputted_value = 1
+
 prev_value = outputted_value
 
 print('Connecting to two WeDo 2.0\'s...')
+
+loop = asyncio.get_event_loop()
+
+def process_q ():
+	global GLOBAL_Q
+	for q in GLOBAL_Q:
+		try:
+			# Write
+			hub, uuid, data = q
+
+			async def _():
+				await hub.client.write_gatt_char(uuid, data, True)
+
+			loop.run_until_complete(_())
+		except ValueError:
+			# Read
+			hub, uuid = q
+			hub.client.read_gatt_char(uuid)
+
+	GLOBAL_Q = []
 
 def callback (sender, data):
 	int_values = [x for x in data]
@@ -25,8 +48,7 @@ def callback (sender, data):
 	
 	global outputted_value
 	
-	outputted_value = convertToNumber(x,y)
-	
+	outputted_value = convertToNumber(x,y)	
 
 	print(f"{sender}: {int_values} / {outputted_value}")
 
@@ -64,21 +86,39 @@ async def run(addr1, add2, loop):
 
 			# await client2.start_notify(SENSOR_VAL_UUID, callback)
 
-			global outputted_value, prev_value
+			global outputted_value, prev_value, GLOBAL_Q
 			
 			try:
 				while True:	
-					# print(queue_final)
 					await asyncio.sleep(0)
 
 					if not outputted_value == prev_value:
 
 						copy = outputted_value
 
+						GLOBAL_Q.append( (client, OUTPUT_COMMAND_UUID, bytearray([0x06,0x04,0x01,copy]) ) )
+						GLOBAL_Q.append( (client2, OUTPUT_COMMAND_UUID, bytearray([0x01, 0x01, 0x01, translate_speed(  copy>0  ) ]) ) )
+						GLOBAL_Q.append( (client2, OUTPUT_COMMAND_UUID, bytearray([0x02, 0x01, 0x01, translate_speed(  (copy>0) * (-1 if copy<4 else 1) ) ]) ) )
+
+
+						for q in GLOBAL_Q:
+							try:
+								# Write
+								c, uuid, data = q
+								await c.write_gatt_char(uuid, data, True)
+
+							except ValueError:
+								# Read
+								c, uuid = q
+								c.read_gatt_char(uuid)
+
+						GLOBAL_Q = []
+
 						# await client.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x06,0x04,0x01,copy]), True)
-						await client2.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x06,0x04,0x01,copy]), True)
-						await client2.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x01, 0x01, 0x01, translate_speed(copy/10) ]), True )
-						await client2.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x02, 0x01, 0x021, translate_speed(-copy/10) ]), True )
+						
+						# await client2.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x06,0x04,0x01,copy]), True)
+						# await client2.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x01, 0x01, 0x01, translate_speed(copy/10) ]), True )
+						# await client2.write_gatt_char(OUTPUT_COMMAND_UUID, bytearray([0x02, 0x01, 0x021, translate_speed(-copy/10) ]), True )
 
 
 					prev_value = outputted_value
@@ -90,7 +130,6 @@ async def run(addr1, add2, loop):
 			except KeyboardInterrupt:
 				print('Stopping notifications...')
 				await client.stop_notify(SENSOR_VAL_UUID)
-				await client2.stop_notify(SENSOR_VAL_UUID)
+				# await client2.stop_notify(SENSOR_VAL_UUID)
 
-loop = asyncio.get_event_loop()
 loop.run_until_complete(run(address1, address2, loop))
