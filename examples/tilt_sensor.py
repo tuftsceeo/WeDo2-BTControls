8be034cc07f9faac382c2c829b867fb0a71d9679
ronitsinha <ronittheawesome@gmail.com>
2019-08-01 14:00:08 -0400

    Fixed REAME typo and started tilt sensor demo

---
+++ examples/tilt_sensor.py
@@ -0,0 +1,7 @@
+# Import WeDo2.py from the parent directory
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from WeDo2 import *
+import asyncio as aio
+
